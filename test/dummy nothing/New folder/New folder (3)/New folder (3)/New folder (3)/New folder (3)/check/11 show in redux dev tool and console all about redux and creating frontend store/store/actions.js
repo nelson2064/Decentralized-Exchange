8b8c9884456d62d3bd7c6714f2dde8afc54a7d4d
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2023-03-24 14:56:58 +0545

    dummy
    dummy

---
+++ test/dummy nothing/New folder/New folder (3)/New folder (3)/New folder (3)/New folder (3)/check/11 show in redux dev tool and console all about redux and creating frontend store/store/actions.js
@@ -0,0 +1,57 @@
+// WEB3
+export function web3Loaded(connection) {
+  return {
+    type: 'WEB3_LOADED',
+    connection
+  }
+}
+
+export function web3AccountLoaded(account) {
+  return {
+    type: 'WEB3_ACCOUNT_LOADED',
+    account
+  }
+}
+
+// TOKEN
+export function tokenLoaded(contract) {
+  return {
+    type: 'TOKEN_LOADED',
+    contract
+  }
+}
+
+// EXCHANGE
+export function exchangeLoaded(contract) {
+  return {
+    type: 'EXCHANGE_LOADED',
+    contract
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//now when you load your web page action will load you can see on console now
+
+
+
+
+
+
+
+
+
+
